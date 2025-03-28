9141d014a793360722007171a5f5dd456e0f732f
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2025-03-28 08:56:53 +0530

    Update Arduino.ino

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,65 +1,77 @@
 #include <SPI.h>
 
-// Motor Control Pins
+// Define motor control pins
 #define IN1 5
 #define IN2 6
 #define IN3 9
 #define IN4 3
 
-volatile char command = 0;
+// SPI communication variable
+volatile byte command = 0;
+volatile bool newData = false;
 
 void setup() {
+    Serial.begin(9600);  // Initialize serial communication
+    
+    // Initialize SPI in slave mode
+    pinMode(MISO, OUTPUT);
+    SPCR |= _BV(SPE);  // Enable SPI in Slave Mode
+    SPI.attachInterrupt();  // Enable SPI interrupt
+    
+    // Initialize motor control pins
     pinMode(IN1, OUTPUT);
     pinMode(IN2, OUTPUT);
     pinMode(IN3, OUTPUT);
     pinMode(IN4, OUTPUT);
-
-    SPI.begin();
-    SPCR |= _BV(SPE);  // Enable SPI Slave
-    SPCR &= ~_BV(MSTR); // Set as Slave
-
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, LOW);
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, LOW);
-
-    SPI.attachInterrupt(); // Enable SPI interrupt
+    
+    Serial.println("Arduino SPI receiver ready");
 }
 
-ISR(SPI_STC_vect) { // SPI interrupt routine
-    command = SPDR; // Read received byte
+// SPI interrupt routine
+ISR(SPI_STC_vect) {
+    command = SPDR;  // Get received byte
+    newData = true;  // Flag that new data is available
+    SPDR = command;  // Echo back received byte for debugging
 }
 
 void loop() {
-    if (command == 'F') {
-        // Move Forward
-        Serial.print("F");
-        digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, HIGH);
-        digitalWrite(IN4, LOW);
-    } 
-    else if (command == 'L') {
-        // Turn Left
-        Serial.print("L");
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, HIGH);
-        digitalWrite(IN3, HIGH);
-        digitalWrite(IN4, LOW);
-    } 
-    else if (command == 'R') {
-        // Turn Right
-        Serial.print("R");
-        digitalWrite(IN1, HIGH);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, HIGH);
-    } 
-    else {
-        // Stop
-        digitalWrite(IN1, LOW);
-        digitalWrite(IN2, LOW);
-        digitalWrite(IN3, LOW);
-        digitalWrite(IN4, LOW);
+    if (newData) {
+        Serial.print("Command: ");
+        Serial.println(command);
+
+        // Control motors based on received command
+        if (command == 'F') {
+            Serial.println("Moving Forward");
+            digitalWrite(IN1, HIGH);
+            digitalWrite(IN2, LOW);
+            digitalWrite(IN3, HIGH);
+            digitalWrite(IN4, LOW);
+        } else if (command == 'L') {
+            Serial.println("Turning Left");
+            digitalWrite(IN1, LOW);
+            digitalWrite(IN2, HIGH);
+            digitalWrite(IN3, HIGH);
+            digitalWrite(IN4, LOW);
+        } else if (command == 'R') {
+            Serial.println("Turning Right");
+            digitalWrite(IN1, HIGH);
+            digitalWrite(IN2, LOW);
+            digitalWrite(IN3, LOW);
+            digitalWrite(IN4, HIGH);
+        } else {
+            Serial.println("Stopping");
+            digitalWrite(IN1, LOW);
+            digitalWrite(IN2, LOW);
+            digitalWrite(IN3, LOW);
+            digitalWrite(IN4, LOW);
+        }
+        
+        newData = false;  // Reset new data flag
     }
 }
+
+
