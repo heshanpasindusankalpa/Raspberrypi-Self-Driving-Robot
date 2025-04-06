8c4f6e503ff51933a6a52bc1d4f1f2c649b6162b
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2025-04-06 22:02:43 +0530

    Update Motor_Testing.ino

--- Arduino/Motor_Testing/Motor_Testing.ino
+++ Arduino/Motor_Testing/Motor_Testing.ino
@@ -1,9 +1,67 @@
-void setup() {
-  // put your setup code here, to run once:
+// Define motor control pins
+#define ENA 9
+#define IN2 8
+#define IN1 7
+#define ENB 6
+#define IN4 4
+#define IN3 2
+#define t 80  // PWM speed (0-255)
 
+void setup() {
+    Serial.begin(9600);  // For monitoring in Serial Monitor
+
+    // Initialize motor control pins
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(ENA, OUTPUT);
+    pinMode(ENB, OUTPUT);
+
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+
+    Serial.println("Motor Test Starting...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // Move Forward
+    Serial.println("Moving Forward");
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, t);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENB, t);
+    delay(2000);
 
+    // Turn Left
+    Serial.println("Turning Left");
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    analogWrite(ENA, t);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENB, t);
+    delay(2000);
+
+    // Turn Right
+    Serial.println("Turning Right");
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    analogWrite(ENA, t);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENB, t);
+    delay(2000);
+
+    // Stop
+    Serial.println("Stopping");
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    delay(2000);
 }
